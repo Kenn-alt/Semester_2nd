bae6067ffb097976c43fea978e2da588fd7175ed
Kenn-alt <ke_altynbek@kbtu.kz>
2025-01-27 12:55:17 +0500

    Arrays

---
+++ lab_3/arrays.py
@@ -0,0 +1,23 @@
+# Arrays
+cars = ['Ford', 'Volvo', 'BMW']
+
+cars.remove("Volvo")
+print(cars)
+
+# count()
+
+fruits = ['apple', 'banana', 'cherry']
+x = fruits.count('apple')
+print(x)
+
+# index()
+x = fruits.index('cherry')
+print(x)
+
+# insert()
+fruits.insert(1, 'orange')
+print(fruits)
+
+# pop()
+fruits.pop(3)
+print(fruits)
