feed72dc39ad621eb128d52bdef7a44488404b9a
Kenn-alt <ke_altynbek@kbtu.kz>
2025-02-03 03:38:24 +0500

    Update

--- lab_3/function1/5.py
+++ lab_3/function1/5.py
@@ -1,8 +1,20 @@
 # A function for permutations of a word
-from itertools import permutations
+# from itertools import permutations
 
-my_str = input("Enter the string: ")
+# my_str = input("Enter the string: ")
 
-permutat = permutations(my_str)
-for p in permutat:
-    print(''.join(p))
+# permutat = permutations(my_str)
+# for p in permutat:
+#     print(''.join(p))
+
+def permutation(my_str, left = ''):
+    if len(my_str) == 0:
+        print(left)  
+    else:
+        for i in range(len(my_str)):
+            remainder = my_str[:i] + my_str[i+1:]
+            permutation(remainder, left + my_str[i])
+
+a = input("Enter the string: ")
+print('The results are:')
+permutation(a)
