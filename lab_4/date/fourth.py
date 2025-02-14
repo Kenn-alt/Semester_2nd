291ba26f24ed5186458a4180ee31d2bd6921081e
Kenn-alt <ke_altynbek@kbtu.kz>
2025-02-14 19:43:25 +0500

    date files

---
+++ lab_4/date/fourth.py
@@ -0,0 +1,21 @@
+import datetime
+
+todayyy = datetime.datetime.now()
+
+# print(todayyy.hour)
+# print(todayyy.minute)
+# print(todayyy.second)
+# print(todayyy.date())
+
+def difference():
+    first_input = input("Enter the first date as follows DD-MM-YYYY HH:MM:SS: ")
+    second_input = input("Enter the second date as follows DD-MM-YYYY HH:MM:SS: ")
+
+    first_date = datetime.datetime.strptime(first_input, "%d-%m-%Y %H:%M:%S")
+    second_date = datetime.datetime.strptime(second_input, "%d-%m-%Y %H:%M:%S")
+
+    diff = abs(first_date - second_date)
+
+    return diff.total_seconds()
+
+print(difference())
