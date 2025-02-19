7bf66e074954d24f62b9c96cb07885d26a0bf24b
Kenn-alt <ke_altynbek@kbtu.kz>
2025-02-19 23:00:10 +0500

    Update

--- lab_5/regexxx/eighth.py
+++ lab_5/regexxx/eighth.py
@@ -4,5 +4,14 @@ import re
 
 my_text = 'There Is Some Sample Text'
 
-x = re.split(r'(?=[A-Z].*)', my_text) # we group words to get the strings that start with 'A-Z' and has zero or more characters after
-print(x[1:])                          # if we don't write '?=', we would erase the found characters [A-Z] ---- we keep them using '?='
+x = re.split(r'(?=[A-Z])', my_text) # we group words to get the strings that start with 'A-Z' and has zero or more characters after
+print(x[1:])                          # if we don't write '?=', we would erase the found characters [A-Z] ---- we keep them using '?=', which 
+                                      # is called 'lookahead assertion' ---> it keeps everything inside the () brackets
+                                      # Example: if we write --- r",(?= )" --- the match is a comma followed by the space. 
+                                      # If we find a match we split our string at this place, but keep everything inside brackets, 
+                                      # which is the space
+
+
+
+# Matches if ... matches next, but doesn’t consume any of the string. This is called a lookahead assertion.
+# For example, Isaac (?=Asimov) will match 'Isaac ' only if it’s followed by 'Asimov'.
