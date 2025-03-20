41eaf014bb7ffe6bfd67e3e76c688bf5c7eaf475
Kenn-alt <ke_altynbek@kbtu.kz>
2025-03-21 04:09:47 +0500

    racer.py without comments

--- lab_8/assignments/racer/racer.py
+++ lab_8/assignments/racer/racer.py
@@ -7,6 +7,8 @@ HEIGHT = 600
 
 COLOR_BLACK = (0, 0, 0)
 COLOR_RED = (255, 0, 0)
+COLOR_GREEN = (0, 255, 0)
+COLOR_BLUE = (0, 0, 255)
 SCORE = 0
 
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
@@ -14,8 +16,12 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 image_background = pygame.image.load('AnimatedStreet.png')
 image_player = pygame.image.load('Player.png')
 image_enemy = pygame.image.load('Enemy.png')
+image_coin = pygame.image.load('coin.png')
+
+scaled_image_coin = pygame.transform.scale(image_coin, (50, 50)) # scaling our coin
 
 font = pygame.font.SysFont('Verdana', 60, True) # getting the font
+font_score = pygame.font.SysFont('Verdana', 40, True)
 font_small = pygame.font.SysFont('Verdana', 20)
 image_game_over = font.render('Game Over', True, COLOR_BLACK) # getting the text with our font
 image_game_over_rect = image_game_over.get_rect(center = (WIDTH // 2, HEIGHT // 2)) # getting the rect object of our text
@@ -63,13 +69,35 @@ class Enemy(pygame.sprite.Sprite):
             self.generate_coord()  # we generate the new random coord for our image to appear
                                    # at the top of the screen
 
+class Coin(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        self.image = scaled_image_coin
+        self.rect = self.image.get_rect()
+        self.positions = [70, 140, 210]
+        self.rect.top = 10
+        self.speed = 5
+        self.random_pos() # setting the initial random position 
+
+    def random_pos(self):
+        self.rect.left = self.positions[random.randint(0, 2)]
+        self.rect.top = 10
+
+    def move(self):
+        self.rect.move_ip(0, self.speed)
+        if self.rect.top > HEIGHT:
+            self.random_pos()
+
 player = Player()
 enemy = Enemy()
+coin = Coin()
 
 all_sprites = pygame.sprite.Group() # grouping all sprites in one place
-all_sprites.add(player, enemy)
+all_sprites.add(player, enemy, coin)
 enemy_sprites = pygame.sprite.Group()
 enemy_sprites.add(enemy)
+coin_sprites = pygame.sprite.Group()
+coin_sprites.add(coin)
 
 clock = pygame.time.Clock()
 FPS = 60
@@ -81,9 +109,9 @@ while running:
             running = False
 
     screen.blit(image_background, image_background_rect)
-    scores = font_small.render(str(SCORE), True, COLOR_BLACK)
-    screen.blit(scores, (10, 10))
-
+    scores = font_small.render(str(SCORE), True, COLOR_BLUE)
+    screen.blit(scores, (WIDTH - 40, 10))
+    
     for entity in all_sprites:
         entity.move()
         screen.blit(entity.image, entity.rect)
@@ -94,11 +122,21 @@ while running:
         sound_crash.play()
         screen.fill(COLOR_RED)
         screen.blit(image_game_over, image_game_over_rect)
-        for entity in all_sprites: # entity.kill() ffectively makes the sprite no longer be drawn or updated 
+        image_total_score = font_score.render('Your Score: ' + str(SCORE), True, COLOR_GREEN)
+        image_total_score_rect = image_total_score.get_rect()
+        image_total_score_rect.center = (WIDTH // 2, HEIGHT // 2 + 100)
+        screen.blit(image_total_score, image_total_score_rect) # showing the total score
+
+        for entity in all_sprites: # entity.kill() effectively makes the sprite no longer be drawn or updated 
             entity.kill() # by those groups
         pygame.display.flip()
         time.sleep(5)
         running = False
-        
+    
+    if pygame.sprite.spritecollideany(player, coin_sprites):
+        SCORE += 10
+        coin.random_pos() # repositioning the coin
+
+
     pygame.display.flip()
     clock.tick(FPS)
