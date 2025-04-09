550daeceda683ab0d444cbfbcb919bb6c7f7c0f2
Kenn-alt <ke_altynbek@kbtu.kz>
2025-04-09 12:39:34 +0500

    update

--- lab_10/first.py
+++ lab_10/first.py
@@ -5,3 +5,20 @@ conn = psycopg2.connect(database = "postgres",
                         host= 'localhost',
                         password = "NaruSaske51>_",
                         port = 5432)
+
+cursor = conn.cursor()
+
+create_table_query = '''
+    CREATE TABLE people (
+    person_id SERIAL NOT NULL PRIMARY KEY, 
+    name varchar(255), 
+    age int
+    );
+'''
+
+extract_table_content_query = '''
+    SELECT * FROM people;
+'''
+
+cursor.execute(extract_table_content_query)
+conn.commit()
