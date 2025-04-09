4eab5a56d57ef9b91977d7fab329da51b5ba62a3
Kenn-alt <ke_altynbek@kbtu.kz>
2025-04-09 09:46:31 +0500

    update

---
+++ lab_10/first.py
@@ -0,0 +1,7 @@
+import psycopg2
+
+conn = psycopg2.connect(database = "postgres", 
+                        user = "postgres", 
+                        host= 'localhost',
+                        password = "NaruSaske51>_",
+                        port = 5432)
